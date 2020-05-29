8f8e54a3b820e6979ead2e5717831b90931187c5
Ioannis Loudaros <johnloudaros@hotmail.com>
2020-05-29 22:32:52 +0300

    Initial Commit

---
+++ Project_Tsakalidis/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Project_Tsakalidis
+//
+//  Created by Ioannis Loudaros on 29/5/20.
+//  Copyright © 2020 Ιωάννης Λουδάρος. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
