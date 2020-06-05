3be6acc85827fb85f48016e5a3687a2e86297386
Ioannis Loudaros <johnloudaros@hotmail.com>
2020-06-05 14:27:25 +0300

    Ξεκαθάρισμα

--- Project_Tsakalidis/johnnylouds.h
+++ Project_Tsakalidis/johnnylouds.h
@@ -3,29 +3,35 @@
 //  Project_Tsakalidis
 //
 //  Εδώ υπάρχει ο parser για την μεταφορά των δεδομένων του αρχείου σε πίνακα
-//  Επίσης εδώ υπάρχει η δήλωση των καινούργιων τύπων "measuret" & "measureh"
+//  Επίσης εδώ υπάρχει η δήλωση των καινούργιων τύπων "time","measuret" & "measureh"
 
 #ifndef johnnylouds_h
 #define johnnylouds_h
 
-
+//Χρόνος, για την εύκολη διαχείρηση των timestamps, ενδιαφέρον έχει το int together, σκοπός του είναι να κάνει ευκολότερο το sorting
 typedef struct{
-    int year
-    int month
-    int day
-    int hour
-    int minute
-    int together
+    int year;
+    int month;
+    int day;
+    int hour;
+    int minute;
+    int together;
 } time;
 
+
+//Τύποι δεδομένων για την καταγραφή των μετρήσεων. Ένας από τους δύο θα μπορούσε να παραληφθεί αλλά χρησιμοποιούνται για λόγους ευαναγνωσιμότητας
 typedef struct{
-    int temp
-    int timestamp
+    int temp;
+    int timestamp;
 } measuret;
 
 typedef struct{
-    int hum
-    int timestamp
+    int hum;
+    int timestamp;
 } measureh;
+
+
+//Parser Δεδομένων, αυτό το τμήμα του κώδικα έχει ως στόχο την ανάγνωση του αρχείου και την εισαγωγή των δεδομένων στο πρόγραμμα
+void
 
 #endif johnnylouds_h
