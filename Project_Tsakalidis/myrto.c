c375fb005eb9a5d305726a8dacd38c71c36a50f4
Ioannis Loudaros <johnloudaros@hotmail.com>
2020-06-10 13:25:27 +0300

    myrto & orestis c files
    euxaristw to vivlio tou kuriou tsakalidi pou me enepneuse

---
+++ Project_Tsakalidis/myrto.c
@@ -0,0 +1,130 @@
+//
+//  myrto.c
+//  Project_Tsakalidis
+//
+//  Created by Ioannis Loudaros on 10/6/20.
+//  Copyright © 2020 Ιωάννης Λουδάρος. All rights reserved.
+//
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+//counting sort
+
+//xreiazomai akoma mia sunarthsh pou tha vriskei to megalutero stoixeio tou pinaka wste na pernaw sthn k to size tou count
+void countingSort(int A[], int count[], int B[]) {
+    int n; //to size twn A kai B
+    int k; //to size tou count
+    int i, j;
+    for(i = 0; i < k; i++){
+        count[i] = 0;
+    }
+    for(j = 0; j < n; j++){
+        count[A[j]] = count[A[j]] + 1;
+    }
+    for(i = 1; i < k; i++){
+        count[i] = count[i] + count[i-1];
+    }
+    for(j = n; j > 0; j--){
+        B[count[A[j]]] = A[j];
+        count[A[j]] = count[A[j]] - 1;
+    }
+}
+
+
+
+
+//heapsort
+
+void adjust(int heapArray[], int i, int n){
+    int j = (2 * i);    //j is left child of i
+    int item = heapArray[i]; //to periexomeno tis thesis i sti metavlhth item
+    
+    //oso uparxei ena stoixeio sti thesi j
+    while(j <= n){
+        //sugkrinw to stoixeio tis j me to epomeno an uparxei
+        if(j < n && heapArray[j] < heapArray[j+1]){
+            j++;
+        }
+        if(item >= heapArray[j]){
+            break;
+        }
+        heapArray[j/2] = heapArray[j];
+        j = 2 * j;
+    }
+    heapArray[j/2] = item;
+}
+
+void heapify(int array[], int n){
+    int i;
+    for(i = n/2; i >= 1; i--){
+        adjust(array, i, n);
+    }
+}
+
+void heapSort(int array[], int n){
+    int i;
+    heapify(array, n);
+    int temp;
+    for(i = n; i >= 2; i--){
+        temp = array[1];
+        array[1] = array[i];
+        adjust(array, 1 ,i-1);
+        array[i] = temp;
+    }
+}
+
+
+
+
+//binary interpolation search
+
+void access(int ts, int a[]){
+    
+    int n; //o arithmos twn timestamps/twn metrhsewn
+    
+    int left = 1;
+    int right = n;
+    
+    int sqr = sqrt(right - left);
+    
+    int next = (((ts - a[left]) / a[right] - a[left]) * (right - left));
+    while(ts != a[next] && left < right){
+        int i=0;
+        if(ts >= a[next]){
+            while(ts > a[next + i * sqr]){
+                i++;
+                right = i * sqr;
+                left = (i - 1) * sqr;
+            }
+        }
+        else if(ts <= a[next]){
+            while(ts > a[next - i * sqr]){
+                i++;
+                right = (i - 1) * sqr;
+                left = i * sqr;
+            }
+        }
+        next = ((ts - a[left]) / (a[right] - a[left]) * (right - left));
+    }
+    if(ts == a[next]){
+        printf("to stoixeio %d vrisketai sti thesi %d\n", ts, next);
+    }
+    else printf("to stoixeio %d den uparxei sti lista\n", ts);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Project_Tsakalidis/orestis.c
@@ -0,0 +1,114 @@
+//
+//  orestis.c
+//  Project_Tsakalidis
+//
+//  Created by Ioannis Loudaros on 10/6/20.
+//  Copyright © 2020 Ιωάννης Λουδάρος. All rights reserved.
+//
+
+#include <stdio.h>
+//
+//  orestis.h
+//  Project_Tsakalidis
+//  Εδώ υπάρχουν οι αλγόριθμοι ταξινόμισης timesort(), bintimesearch(), intertimesearch()
+//  !!Φρόντισε να υπάρχει μια μικρή περιγραφή του τι κάνει ο αλγόριθμος πριν την δήλωση του <3
+
+#ifndef orestis_h
+#define orestis_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "johnnylouds.h"
+
+
+
+//TIMESORT
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+  
+void timesort(logh *p, int n) //kanw sort ton pinaka me vash ta timestamps gia na mporw meta na kanw binary search poy ematha apo to vivlio toy kyrioy tsakalidi
+{
+    int i, j, min_idx;
+  
+    for (i = 0; i < n-1; i++)
+    {
+        min_idx = i;
+        for (j = i+1; j < n; j++)
+        if (p[j].time.together < p[min_idx].time.together)
+            min_idx = j;
+  
+        swap(&p[min_idx].time.together, &p[i].time.together);
+    }
+}
+
+//BINARY
+
+int binarySearch(logh *p, int x, int n) //psaxnw me vash to timestamp me th methodo binary search poy ematha apo to vivlio toy kyrioy tsakalidi
+{
+    int high = n-1;
+    int low = 0;
+    int mid;
+    
+    if (high >= low)
+    {
+        mid = low + (high - low)/2;
+
+        if (p[mid].time.together == x) //an einai sth mesh parto
+            return mid;
+
+        else if (p[mid].time.together > x) //psaxnw aristera
+            return binarySearch(logh *p, x, low, mid-1)
+
+        else //psaxnw deksia
+            return binarySearch(logh *p, x, mid + 1, high)
+
+    else //alliws ton poylo
+        return -1
+    }
+}
+
+//INTERPOLATION
+
+int interpolationSearch(logh *p, int n, int x)
+{
+    int low = 0;
+    int high = (n - 1);
+  
+    while (low <= high && x >= p[low].time.together && x <= p[high].time.together)
+    {
+        if (low == high)
+        {
+            if (p[low].time.together == x) return low;
+            return -1;
+        }
+
+        //vriskw th thesh me ton typo apo to vivlio toy kyrioy tsakalidi
+        int pos = low + (((double)(high - low) /
+                          (p[high].time.together - p[low]).time.together) * (x - p[low].time.together));
+  
+        //an to vrikame me th mia oyaoy
+        if (p[pos].time.together == x)
+            return pos;
+  
+        //an to x einai megalytero apo ayto poy vrikame des deksia
+        if (p[pos].time.together < x)
+            low = pos + 1;
+  
+        //an to x einai mikrotero apo ayto poy vrikame des aristera
+        else
+            high = pos - 1;
+    }
+    return -1;
+}
+
+
+
+
+
+
+#endif orestis_h
